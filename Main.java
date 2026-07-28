b58d4e41a5c30c6b53e48c7b7b690e54ac0ca8b3
Niraj Yadav <nirajyadav15867@gmail.com>
2026-07-28 16:18:29 +0530

    Day 1 - My First Java Program

---
+++ Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args){
+        System.out.println("Hello World");
+       int a = 45;
+       int b = 65;
+       int sum = a + b;
+       int mult = a * b;
+       float div = b / a ;
+        System.out.println(sum);
+        System.out.println(mult);
+        System.out.println(div);
+
+    }
+
+
+}
+
+
+
+
