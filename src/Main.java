b0f7cb448f642dc7f72e8d9a8f529f5c355502e1
Niraj Yadav <nirajyadav15867@gmail.com>
2026-07-29 17:29:50 +0530

    Day 2 : Added variables, Scanner input, and arithmetic operations

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+ public class Main {
+   public static void main(String[] args){
+       int a = 10;
+        int b = 15;
+      int sum = a+b;
+         System.out.println(sum);
+        int sub = b-a;
+       System.out.println(sub);
+        int multi = a*b;
+       System.out.println(multi);
+       int ans = (a*b)/(b-a);
+        System.out.println(ans);
+   }
+ }
+
+//import java.util.*;
+//public class Main {
+  //  public static void main(String[] args){
+    //    Scanner sc = new Scanner(System.in);
+    //    int a = sc.nextInt();
+    //    int b = sc.nextInt();
+    //    int sum = a+b;
+    //    System.out.println(sum);
+   // }
+//}
