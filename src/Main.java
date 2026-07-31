c8a0947f142906af24865b09091bc029dd54dd6e
Niraj Yadav <nirajyadav15867@gmail.com>
2026-07-31 20:00:30 +0530

    Day 4: Java for loop fundamentals

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,26 @@
-public class Main {
-    public static void main(String[] args){
-        String name = "Niraj";
-        int age= 21;
-        float height = 174.5f;
-        char bloodGroup = 'B';
-        boolean student = true;
+public class Main{
+    public static void main(String[]args){
+        // Print the numbers from 0 to 10
+        for(int i=0; i<=10; i++){
+           System.out.println(i);
+            // Print the Even number from 2 to 100
+        for(int j=2; j<=100; j+=2){
+            System.out.println(j);
+            //Print the multiplication of 5
+            int number =5;
+        for(int k=5; k<=10; k++){
+            System.out.println(number + " x " + k +"=" + (number * k));
+            //Print the odd number from 1 to 100
+            for(int l=1; l<=100; l+=2){
+                System.out.println(l);
+          //    Print the Reverse Counting from 100 to 1
+             for(int m=100; m>=1; m--){
+                 System.out.println(m);
+        }
+        }
+        }
+        }
+       }
 
-        System.out.println("Name:" + name);
-        System.out.println("Age:" +age);
-        System.out.println("Height:" + height);
-        System.out.println("Blood Group Initial:" + bloodGroup);
-        System.out.println("Student:" + student);
     }
 }
