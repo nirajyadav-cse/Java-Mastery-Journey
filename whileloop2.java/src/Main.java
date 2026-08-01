c35a188f1cc133e06cb46ddaf3bb2c755435440d
Niraj Yadav <nirajyadav15867@gmail.com>
2026-08-01 19:51:02 +0530

    Day 5: Multiplication Table using while loop

---
+++ whileloop2.java/src/Main.java
@@ -0,0 +1,21 @@
+
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+
+        int i = 1;
+
+        while (i <= 10) {
+            System.out.println(n + " x " + i + " = " + (n * i));
+            i++;
+        }
+
+        sc.close();
+    }
+}
