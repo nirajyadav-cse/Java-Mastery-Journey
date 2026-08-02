5313cbe22b065c100477a71ef06f0905fd83a320
Niraj Yadav <nirajyadav15867@gmail.com>
2026-08-02 22:06:45 +0530

    Day 6: Print Sum of First N natural numbers using do-while loop

---
+++ SumFirstN-NaturalNumber6.do-while-loop.java/src/Main.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter N: ");
+        int n = sc.nextInt();
+
+        int i = 1;
+        int sum = 0;
+
+        do {
+            sum += i;
+            i++;
+        } while (i <= n);
+
+        System.out.println("Sum = " + sum);
+
+    }
+}
