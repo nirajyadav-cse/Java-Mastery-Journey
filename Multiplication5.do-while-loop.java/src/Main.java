2c11e3b9166b0c278079cf10fdcb22fc92a4e63e
Niraj Yadav <nirajyadav15867@gmail.com>
2026-08-02 22:05:24 +0530

    Day 6: Print Multiplication Table using do-while loop

---
+++ Multiplication5.do-while-loop.java/src/Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number: ");
+        int n = sc.nextInt();
+
+        int i = 1;
+
+        do {
+            System.out.println(n + " x " + i + " = " + (n * i));
+            i++;
+        } while (i <= 10);
+
+    }
+}
