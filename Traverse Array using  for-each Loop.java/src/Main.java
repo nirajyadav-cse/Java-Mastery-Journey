b81c22a277ee218d18960f224eea4524ac5d8f8e
Niraj Yadav <nirajyadav15867@gmail.com>
2026-08-05 16:58:28 +0530

    Day 9: Print Traverse Array using for-each loop

---
+++ Traverse Array using  for-each Loop.java/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int[] arr = {2, 4, 6, 8, 10};
+
+        for (int num : arr) {
+            System.out.println(num);
+        }
+    }
+}
