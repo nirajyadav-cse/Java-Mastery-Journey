7339b2497d1673dfebfa28ba48dcf9ad72b96591
Niraj Yadav <nirajyadav15867@gmail.com>
2026-08-05 17:00:20 +0530

    Day 9: Print First and Last Element

---
+++ Print First and Last Element.java/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int[] arr = {10, 20, 30, 40, 50};
+
+        System.out.println("First Element = " + arr[0]);
+        System.out.println("Last Element = " + arr[arr.length - 1]);
+    }
+}
