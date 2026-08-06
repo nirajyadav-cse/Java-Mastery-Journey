b59a9d7ef31afa5517338a6762e64ce42a179f14
Niraj Yadav <nirajyadav15867@gmail.com>
2026-08-06 21:02:46 +0530

    Day 10: Print Count Even Number in Array

---
+++ Count Even Number .java/src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int[] arr = {10, 15, 20, 25, 30, 35};
+
+        int count = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+
+            if (arr[i] % 2 == 0) {
+                count++;
+            }
+
+        }
+
+        System.out.println("Even Numbers = " + count);
+    }
+}
