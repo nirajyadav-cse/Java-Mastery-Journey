c73ed4cb32bc3cbe92785f6f8fccd49203e9549e
Niraj Yadav <nirajyadav15867@gmail.com>
2026-08-07 21:41:14 +0530

    Day 11: Print to find the second smallest element in array

---
+++ Find Second Smallest Element .java/src/Main.java
@@ -0,0 +1,23 @@
+
+public class Main {
+    public static void main(String[] args) {
+
+        int[] arr = {12, 45, 7, 89, 56};
+
+        int smallest = arr[0];
+        int secondSmallest = arr[0];
+
+        for (int i = 1; i < arr.length; i++) {
+
+            if (arr[i] < smallest) {
+                secondSmallest = smallest;
+                smallest = arr[i];
+            } else if (arr[i] < secondSmallest && arr[i] != smallest) {
+                secondSmallest = arr[i];
+            }
+
+        }
+
+        System.out.println("Second Smallest = " + secondSmallest);
+    }
+}
