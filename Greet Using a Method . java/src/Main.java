e552403129f0f33af57a5fb8900eeae25313df66
Niraj Yadav <nirajyadav15867@gmail.com>
2026-08-08 20:41:47 +0530

    Day 12: Greet Using a Method

---
+++ Greet Using a Method . java/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    static void greet() {
+        System.out.println("Welcome to my Java journey!");
+    }
+
+    public static void main(String[] args) {
+
+        greet();
+
+    }
+}
