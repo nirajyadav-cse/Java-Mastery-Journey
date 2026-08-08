d9b5e0a422642969ea33004933a9c0e9829450fd
Niraj Yadav <nirajyadav15867@gmail.com>
2026-08-08 20:41:00 +0530

    Day 12: Create and Call a Simple Method

---
+++ Create and Call a Simple Method .java/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    static void message() {
+        System.out.println("Hello, welcome to Java!");
+    }
+
+    public static void main(String[] args) {
+
+        message();
+
+    }
+}
