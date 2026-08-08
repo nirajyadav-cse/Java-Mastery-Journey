1ce549fbc76e300b551ec402d10a3786bf951681
Niraj Yadav <nirajyadav15867@gmail.com>
2026-08-08 20:43:34 +0530

    Day 12: Check Even or Odd using a Method

---
+++ Check Even or Odd using a Method . java/src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+
+    static void evenOdd() {
+
+        int number = 15;
+
+        if (number % 2 == 0) {
+            System.out.println("Even Number");
+        } else {
+            System.out.println("Odd Number");
+        }
+    }
+
+    public static void main(String[] args) {
+
+        evenOdd();
+
+    }
+}
