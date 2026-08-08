b9b57308d26c1f920626ef9e9fd9083555db2d95
Niraj Yadav <nirajyadav15867@gmail.com>
2026-08-08 20:45:25 +0530

    Day 12: Find Square of a Number Using a Method

---
+++ Find Square of a Number Using a Method .java/src/Main.java
@@ -0,0 +1,18 @@
+
+public class Main {
+
+    static void square() {
+
+        int number = 8;
+
+        int result = number * number;
+
+        System.out.println("Square = " + result);
+    }
+
+    public static void main(String[] args) {
+
+        square();
+
+    }
+}
