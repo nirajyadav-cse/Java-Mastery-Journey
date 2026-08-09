d394d2eab579a17fb8878b6bed04e353a73eea7a
Niraj Yadav <nirajyadav15867@gmail.com>
2026-08-09 21:34:23 +0530

    Day 13: Method with multiple parameters

---
+++ Method with Multiple Parameters .java/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    static void studentInfo(String name, int age) {
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+    }
+
+    public static void main(String[] args) {
+
+        studentInfo("Niraj", 21);
+
+    }
+}
