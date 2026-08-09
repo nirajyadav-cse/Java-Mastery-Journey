ff3e6faa5f7407f7cfc0abb08d3bfe3d279ce6eb
Niraj Yadav <nirajyadav15867@gmail.com>
2026-08-09 21:35:28 +0530

    Day 13: Add two number using a method

--- Add Two Numbers Using a Method .java/src/Main.java
+++ Add Two Numbers Using a Method .java/src/Main.java
@@ -1,18 +1,14 @@
 public class Main {
 
-    static void add() {
-
-        int a = 10;
-        int b = 20;
-
-        int sum = a + b;
-
-        System.out.println("Sum = " + sum);
+    static int add(int a, int b) {
+        return a + b;
     }
 
     public static void main(String[] args) {
 
-        add();
+        int result = add(10, 20);
+
+        System.out.println("Sum = " + result);
 
     }
 }
