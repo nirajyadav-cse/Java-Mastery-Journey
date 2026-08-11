de096cc30fdeac7f58b974234402da9857e5ee60
Niraj Yadav <nirajyadav15867@gmail.com>
2026-08-11 21:39:39 +0530

    Day 15: Print Each Character of a String

---
+++ Print Each Character of a String .java/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        String word = "NIRAJ";
+
+        for (int i = 0; i < word.length(); i++) {
+
+            System.out.println(word.charAt(i));
+
+        }
+    }
+}
