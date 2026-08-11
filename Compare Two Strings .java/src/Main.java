2cdb7934296063a78b3ba53e4de49acecb22eeea
Niraj Yadav <nirajyadav15867@gmail.com>
2026-08-11 21:43:46 +0530

    Day 15: Compare Two Strings

---
+++ Compare Two Strings .java/src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        String first = "Java";
+        String second = "Java";
+
+        if (first.equals(second)) {
+
+            System.out.println("Both Strings are equal");
+
+        } else {
+
+            System.out.println("Strings are different");
+
+        }
+    }
+}
