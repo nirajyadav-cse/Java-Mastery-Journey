2d09efaf9f7ee6d22403ae1865375be9dcd82b07
Niraj Yadav <nirajyadav15867@gmail.com>
2026-08-11 21:44:32 +0530

    Day 15: Join Two Sting

---
+++ Join Two String .java/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        String firstName = "Niraj";
+        String lastName = "Yadav";
+
+        String fullName = firstName + " " + lastName;
+
+        System.out.println(fullName);
+
+    }
+}
