4fc4cc3f7d815dbb66727720b62c2e6639e13ca8
Niraj Yadav <nirajyadav15867@gmail.com>
2026-08-12 22:36:04 +0530

    Day 16: Count Digits and Consonants

---
+++ Count Vowels and Consonants .java/src/Main.java
@@ -0,0 +1,28 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        String text = "education";
+
+        int vowels = 0;
+        int consonants = 0;
+
+        for (int i = 0; i < text.length(); i++) {
+
+            char ch = Character.toLowerCase(text.charAt(i));
+
+            if (ch == 'a' || ch == 'e' || ch == 'i' ||
+                    ch == 'o' || ch == 'u') {
+
+                vowels++;
+
+            } else if (ch >= 'a' && ch <= 'z') {
+
+                consonants++;
+            }
+        }
+
+        System.out.println("Vowels = " + vowels);
+        System.out.println("Consonants = " + consonants);
+    }
+}
