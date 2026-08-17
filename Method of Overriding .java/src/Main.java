0b788fbae1310fd7d575baaf091239cf84114b74
Niraj Yadav <nirajyadav15867@gmail.com>
2026-08-17 21:37:14 +0530

    Day 21:Method of Overriding

---
+++ Method of Overriding .java/src/Main.java
@@ -0,0 +1,24 @@
+class Animal {
+
+    void sound() {
+        System.out.println("Animal makes a sound");
+    }
+}
+
+class Dog extends Animal {
+
+    @Override
+    void sound() {
+        System.out.println("Dog barks");
+    }
+}
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Dog d1 = new Dog();
+
+        d1.sound();
+    }
+}
