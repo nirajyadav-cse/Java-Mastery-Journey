7196f8cd1aaa5d95c3aa70ee2285103ef9ef6507
Niraj Yadav <nirajyadav15867@gmail.com>
2026-08-20 20:24:46 +0530

    Day 23: Calculator - Method Overloading

---
+++ Calculator - Method Overloading .java/src/Main.java
@@ -0,0 +1,28 @@
+class Calculator {
+
+    int add(int a, int b) {
+        return a + b;
+    }
+
+    int add(int a, int b, int c) {
+        return a + b + c;
+    }
+
+    double add(double a, double b) {
+        return a + b;
+    }
+}
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Calculator c = new Calculator();
+
+        System.out.println("Addition of 2 numbers: " + c.add(10, 20));
+
+        System.out.println("Addition of 3 numbers: " + c.add(10, 20, 30));
+
+        System.out.println("Addition of decimal numbers: " + c.add(10.5, 20.5));
+    }
+}
