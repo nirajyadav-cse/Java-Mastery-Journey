e2fae2727d85e096e2b0effd22821987d761e03a
Niraj Yadav <nirajyadav15867@gmail.com>
2026-08-25 21:51:42 +0530

    Day 27: Find Largest number

---
+++ Find Largest number .java/src/Main.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        ArrayList<Integer> numbers = new ArrayList<>();
+
+        numbers.add(25);
+        numbers.add(10);
+        numbers.add(75);
+        numbers.add(40);
+        numbers.add(60);
+
+        int largest = numbers.get(0);
+
+        for (int number : numbers) {
+
+            if (number > largest) {
+                largest = number;
+            }
+        }
+
+        System.out.println("Numbers: " + numbers);
+        System.out.println("Largest number: " + largest);
+    }
+}
