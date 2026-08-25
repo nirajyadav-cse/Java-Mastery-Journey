7fec0ed2a813adb51f4871ed94d7347e11f38be1
Niraj Yadav <nirajyadav15867@gmail.com>
2026-08-25 21:49:44 +0530

    Day 27: Find ArrayList Size

---
+++ Find ArrayList Size .java/src/Main.java
@@ -0,0 +1,19 @@
+
+import java.util.ArrayList;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        ArrayList<String> names = new ArrayList<>();
+
+        names.add("Niraj");
+        names.add("Rahul");
+        names.add("Aman");
+        names.add("Rohit");
+
+        System.out.println("Names: " + names);
+
+        System.out.println("Number of names: " + names.size());
+    }
+}
