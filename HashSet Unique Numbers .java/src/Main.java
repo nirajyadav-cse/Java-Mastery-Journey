2cbd76dcbfd8c4480372f25e2b0372fd01b27bee
Niraj Yadav <nirajyadav15867@gmail.com>
2026-08-26 21:04:54 +0530

    Day 28: HashSet Unique Numbers

---
+++ HashSet Unique Numbers .java/src/Main.java
@@ -0,0 +1,18 @@
+import java.util.HashSet;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        HashSet<Integer> numbers = new HashSet<>();
+
+        numbers.add(10);
+        numbers.add(20);
+        numbers.add(10);
+        numbers.add(30);
+        numbers.add(20);
+        numbers.add(40);
+
+        System.out.println("Numbers: " + numbers);
+    }
+}
