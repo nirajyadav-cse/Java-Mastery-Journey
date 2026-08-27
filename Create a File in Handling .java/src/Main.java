10f8bf685bf6d855e6c2bd087109f5fbd2cec4a6
Niraj Yadav <nirajyadav15867@gmail.com>
2026-08-27 22:58:05 +0530

    Day 29: Create a File in Handling

---
+++ Create a File in Handling .java/src/Main.java
@@ -0,0 +1,23 @@
+import java.io.File;
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        File file = new File("student.txt");
+
+        try {
+
+            if (file.createNewFile()) {
+                System.out.println("File created successfully");
+            } else {
+                System.out.println("File already exists");
+            }
+
+        } catch (IOException e) {
+
+            System.out.println("An error occurred");
+        }
+    }
+}
