5caa5c32502bafa850bf5fe172c8311dfb61f4fb
Niraj Yadav <nirajyadav15867@gmail.com>
2026-08-29 21:27:54 +0530

    Day 31: Sum Using Reduce

---
+++ Sum Using Reduce .java/src/Main.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        ArrayList<Integer> numbers = new ArrayList<>();
+
+        numbers.add(10);
+        numbers.add(20);
+        numbers.add(30);
+        numbers.add(40);
+        numbers.add(50);
+
+        int sum = numbers.stream()
+                .reduce(0, (a, b) -> a + b);
+
+        System.out.println("Sum: " + sum);
+    }
+}
